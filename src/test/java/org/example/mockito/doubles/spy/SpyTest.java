90449a6053d9af3db67897c8d629ea3e09b7f3c6
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-20 12:53:30 +0200

    Mockito (Spy) Example

--- src/test/java/org/example/mockito/doubles/spy/SpyTest.java
+++ src/test/java/org/example/mockito/doubles/spy/SpyTest.java
@@ -1,24 +1,27 @@
-//package org.example.mockito.doubles.spy;
-//
-//import org.example.mockito.doubles.mock.BookRepoMock;
-//import org.junit.jupiter.api.Test;
-//
-//import java.time.LocalDate;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class SpyTest {
-//
-//    @Test
-//    public void demoMock() {
-//        BookRepoMock bookRepoMock = new BookRepoMock();
-//        BookService bookService = new BookService(bookRepoMock);
-//        Book book1 = new Book("1234", "Mockito3", 250, LocalDate.now());
-//        Book book2 = new Book("1235", "jUnit5", 150, LocalDate.now());
-//
-//        bookService.addBook(book1);
-//        bookService.addBook(book2);
-//
-//    }
-//}
+package org.example.mockito.doubles.spy;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class SpyTest {
+
+    @Test
+    public void demoMockSpy() {
+        BookRepo bookRepoMock = spy(BookRepo.class);
+        BookService bookService = new BookService(bookRepoMock);
+        Book book1 = new Book("1234", "Mockito3", 500, LocalDate.now());
+        Book book2 = new Book("1235", "jUnit5", 200, LocalDate.now());
+
+        bookService.addBook(book1);
+        bookService.addBook(book2);
+
+        verify(bookRepoMock, times(1)).save(book2);
+        verify(bookRepoMock, times(0)).save(book1);
+
+    }
+}
