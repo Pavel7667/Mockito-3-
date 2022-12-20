dd69d2b1261a5f27d7701da5da658a118759bfec
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-20 11:55:58 +0200

    Mockito (Stub) Example

--- src/test/java/org/example/mockito/doubles/stub/StubTest.java
+++ src/test/java/org/example/mockito/doubles/stub/StubTest.java
@@ -2,9 +2,13 @@ package org.example.mockito.doubles.stub;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StubTest {
 
@@ -15,6 +19,27 @@ public class StubTest {
         List<Book> newBooks = bookService.getNewBooksWithAppliedDiscount(10, 7);
 
         assertEquals(2, newBooks.size());
-        assertEquals(450, newBooks.get(0).getPrice());
+
+    }
+
+    @Test
+    public void demoStubMock() {
+        BookRepoStub bookRepoStub = mock(BookRepoStub.class);
+        BookService bookService = new BookService(bookRepoStub);
+
+        Book book1 = new Book("1234", "Mockito3", 500, LocalDate.now());
+        Book book2 = new Book("1235", "jUnit5", 500, LocalDate.now());
+
+        List<Book> newBooksTwo = new ArrayList<>();
+        newBooksTwo.add(book1);
+        newBooksTwo.add(book1);
+
+        when(bookRepoStub.findNewBooks(7)).thenReturn(newBooksTwo);
+
+        List<Book> newBookWithApplicationDiscount =
+                bookService.getNewBooksWithAppliedDiscount(10, 7);
+
+        assertEquals(2, newBooksTwo.size());
+
     }
 }
