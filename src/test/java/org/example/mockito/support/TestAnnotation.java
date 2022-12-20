cb6f0559e54e8ebcca8f730cb767b34f82945829
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-20 13:13:28 +0200

    Mockito @Annotation Example part 2

--- src/test/java/org/example/mockito/support/TestAnnotation.java
+++ src/test/java/org/example/mockito/support/TestAnnotation.java
@@ -1,7 +1,10 @@
 package org.example.mockito.support;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -10,15 +13,17 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class TestAnnotation {
 
+    @InjectMocks
+    private BookService bookService;
     @Mock
     private BookRepo bookRepo;
 
+
     @Test
     public void demoCreateTest() {
-
-        BookService bookService = new BookService(bookRepo);
 
         Book book1 = new Book("1234", "Mockito3", 500, LocalDate.now());
         Book book2 = new Book("1235", "jUnit5", 500, LocalDate.now());
