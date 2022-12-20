b42785772da4afffa65b445ac1e87c855da93ee6
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-20 11:21:04 +0200

    Mockito (Fake) Example

--- src/test/java/org/example/mockito/doubles/fake/Test_Double_Fake.java
+++ src/test/java/org/example/mockito/doubles/fake/Test_Double_Fake.java
@@ -1,10 +1,14 @@
 package org.example.mockito.doubles.fake;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class Test_Double_Fake {
 
@@ -17,5 +21,23 @@ public class Test_Double_Fake {
         bookService.addBook(new Book("1235", "jUnit5", 150, LocalDate.now()));
 
         assertEquals(2, bookService.findNumberOfBook());
+    }
+
+    @Test
+    public void testFakeWithMockito() {
+        BookRepo bookRepo = Mockito.mock(BookRepo.class);
+        BookService bookService = new BookService(bookRepo);
+
+        Book book1 = new Book("1234", "Mockito3", 250, LocalDate.now());
+        Book book2 = new Book("1235", "jUnit5", 150, LocalDate.now());
+
+        Collection<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book1);
+
+        when(bookRepo.findAll()).thenReturn(books);
+
+        assertEquals(2, bookService.findNumberOfBook());
+
     }
 }
