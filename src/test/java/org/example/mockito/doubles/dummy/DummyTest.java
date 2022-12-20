b4f8a997ca3dc2ab976a7d067ec2e827b3c7e4d7
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-12-20 11:34:09 +0200

    Mockito (Dummy) Example

--- src/test/java/org/example/mockito/doubles/dummy/DummyTest.java
+++ src/test/java/org/example/mockito/doubles/dummy/DummyTest.java
@@ -4,8 +4,12 @@ package org.example.mockito.doubles.dummy;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DummyTest {
 
@@ -17,6 +21,24 @@ public class DummyTest {
 
         bookService.addBook(new Book("1234", "Mockito3", 250, LocalDate.now()));
         bookService.addBook(new Book("1235", "jUnit5", 150, LocalDate.now()));
+
+        assertEquals(2, bookService.findNumberOfBook());
+    }
+
+    @Test
+    public void testFakeMockito() {
+        BookRepo bookRepo = mock(BookRepo.class);
+        EmailService emailService = mock(EmailService.class);
+        BookService bookService = new BookService(bookRepo, emailService);
+
+        Book book1 = new Book("1234", "Mockito3", 250, LocalDate.now());
+        Book book2 = new Book("1235", "jUnit5", 150, LocalDate.now());
+
+        Collection<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book1);
+
+        when(bookRepo.findAll()).thenReturn(books);
 
         assertEquals(2, bookService.findNumberOfBook());
     }
